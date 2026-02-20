5941552d43d08a10c09241f6e9b92894508695ba
DIVYA <152860478+Divi-1026@users.noreply.github.com>
2026-02-20 23:33:26 +0530

    Update redis.js

--- Backend/src/config/redis.js
+++ Backend/src/config/redis.js
@@ -1,34 +1,34 @@
-// const { createClient } = require('redis');
-// require('dotenv').config({ path: __dirname + '/../../.env' });
-
-// const redisClient = createClient({
-//   username: 'default', // optional for Redis Cloud, but safe to include
-//   password: process.env.REDIS_PASS,
-//    socket: {
-//         host: 'redis-16658.crce217.ap-south-1-1.ec2.redns.redis-cloud.com',
-//         port: 16658
-//     },
-// });
-
-// redisClient.on('error', (err) => console.error('Redis Client Error', err));
-
-// module.exports = redisClient;
 const { createClient } = require('redis');
 require('dotenv').config({ path: __dirname + '/../../.env' });
-const client = createClient({
-    username: 'default',
-    password: '4rSx48fFS24pfjbuiM29UL0fegeNUBtK',
-    socket: {
-        host: 'redis-15157.c273.us-east-1-2.ec2.cloud.redislabs.com',
+
+const redisClient = createClient({
+  username: 'default', // optional for Redis Cloud, but safe to include
+  password: process.env.REDIS_PASS,
+   socket: {
+         host: 'redis-15157.c273.us-east-1-2.ec2.cloud.redislabs.com',
         port: 15157
-    }
+    },
 });
 
-client.on('error', err => console.log('Redis Client Error', err));
+redisClient.on('error', (err) => console.error('Redis Client Error', err));
 
-await client.connect();
+module.exports = redisClient;
+// const { createClient } = require('redis');
+// require('dotenv').config({ path: __dirname + '/../../.env' });
+// const client = createClient({
+//     username: 'default',
+//     password: '4rSx48fFS24pfjbuiM29UL0fegeNUBtK',
+//     socket: {
+//         host: 'redis-15157.c273.us-east-1-2.ec2.cloud.redislabs.com',
+//         port: 15157
+//     }
+// });
 
-await client.set('foo', 'bar');
-const result = await client.get('foo');
-console.log(result)  // >>> bar
+// client.on('error', err => console.log('Redis Client Error', err));
+
+// await client.connect();
+
+// await client.set('foo', 'bar');
+// const result = await client.get('foo');
+// console.log(result)  // >>> bar
 
