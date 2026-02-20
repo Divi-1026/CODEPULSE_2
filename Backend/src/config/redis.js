48c5684580ee301a5e5a2a9a80f81d1a264d0a0a
DIVYA <152860478+Divi-1026@users.noreply.github.com>
2026-02-20 21:33:07 +0530

    Update redis.js

--- Backend/src/config/redis.js
+++ Backend/src/config/redis.js
@@ -1,15 +1,40 @@
+// const { createClient } = require('redis');
+// require('dotenv').config({ path: __dirname + '/../../.env' });
+
+// const redisClient = createClient({
+//   username: 'default', // optional for Redis Cloud, but safe to include
+//   password: process.env.REDIS_PASS,
+//    socket: {
+//         host: 'redis-16658.crce217.ap-south-1-1.ec2.redns.redis-cloud.com',
+//         port: 16658
+//     },
+// });
+
+// redisClient.on('error', (err) => console.error('Redis Client Error', err));
+
+// module.exports = redisClient;
+
 const { createClient } = require('redis');
 require('dotenv').config({ path: __dirname + '/../../.env' });
 
 const redisClient = createClient({
-  username: 'default', // optional for Redis Cloud, but safe to include
+  username: 'default',
   password: process.env.REDIS_PASS,
-   socket: {
-        host: 'redis-16658.crce217.ap-south-1-1.ec2.redns.redis-cloud.com',
-        port: 16658
-    },
+  socket: {
+    host: 'redis-16658.crce217.ap-south-1-1.ec2.redns.redis-cloud.com',
+    port: 16658
+  }
 });
 
 redisClient.on('error', (err) => console.error('Redis Client Error', err));
+
+async function connectRedis() {
+  if (!redisClient.isOpen) {
+    await redisClient.connect();
+    console.log("Redis Connected");
+  }
+}
+
+connectRedis();
 
 module.exports = redisClient;
